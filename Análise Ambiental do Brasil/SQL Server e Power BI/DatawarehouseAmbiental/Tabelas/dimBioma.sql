90f700dd4cebca5b53d5642dbd8f0fd5c7bc5008
Tayhana Marçal Rangel da Fonseca <tayhanarangel@gmail.com>
2023-09-01 21:13:48 -0300

    Update

--- Análise Ambiental do Brasil/SQL Server e Power BI/DatawarehouseAmbiental/Tabelas/dimBioma.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[dimBioma]
-(
-	[id_bioma] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [desc_bioma] NVARCHAR(50) NULL
-)
--- Análise Ambiental do Brasil/SQL Server e Power BI/DatawarehouseAmbiental/Tabelas/dimLocalizacao.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[dimLocalizacao]
-(
-	[id_cidade] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [desc_cidade] NVARCHAR(50) NULL,
-    [id_estado] NVARCHAR(50) NULL, 
-    [desc_estado] NVARCHAR(50) NULL, 
-    [sigla] NVARCHAR(50) NULL 
-)
